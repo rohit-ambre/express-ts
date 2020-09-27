a44d9ab790771a833753300a3fbbe81768d32711
Rohit Ambre <rohit.a@servify.in>
2020-09-28 02:34:41 +0530

    feat:

---
+++ server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
