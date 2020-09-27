a425b18b4d0496aa84c97a76ca3ac0983cf53d0e
Rohit Ambre <rohit003.ambre@gmail.com>
2020-09-28 02:46:54 +0530

    feat: added dotenv for ENV support

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+require('dotenv').config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
