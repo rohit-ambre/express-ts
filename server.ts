43f589bbc65398abf6e69236d14090e9febf1017
Rohit Ambre <rohit003.ambre@gmail.com>
2020-09-29 01:44:39 +0530

    feat: added express body parser

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.listen(PORT, () => {
