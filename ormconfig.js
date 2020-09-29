1e37aa044ab0ad3e3380a0d4259f956483447fb7
Rohit Ambre <rohit003.ambre@gmail.com>
2020-09-30 00:33:21 +0530

    feat: used env vars and connected DB

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
    "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
-   "username": "test",
-   "password": "test",
-   "database": "test",
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USER,
+   "password": process.env.DB_PASSWORD,
+   "database": process.env.DB_NAME,
    "synchronize": true,
    "logging": false,
    "entities": [
