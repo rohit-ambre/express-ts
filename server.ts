5fed27c91de1f9b9df768e07d7b0c2c134386fc5
Rohit Ambre <rohit003.ambre@gmail.com>
2020-10-04 13:41:50 +0530

    refactor: moved DB connection to server file

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Application } from 'express';
 import express = require('express');
+import { createConnection } from 'typeorm';
 import apiRoutes from './src/routes';
+import 'reflect-metadata';
 
 require('dotenv').config();
 
@@ -10,11 +12,19 @@ const PORT: Number = parseInt(process.env.PORT, 10);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', apiRoutes);
+createConnection()
+  .then(() => {
+    console.log('Database connected');
+    app.use('/api', apiRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+    app.get('/', (req: Request, res: Response) => {
+      res.send('Express + TypeScript Server');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    throw new Error(`Unable to connect to the database:' ${error.message}`);
+  });
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
