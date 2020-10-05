a04cbcd4b9a61c1ed0ac6c2aba23f37d15514a70
Rohit Ambre <rohit003.ambre@gmail.com>
2020-10-06 01:11:24 +0530

    feat: completed signup controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../entity/User';
 
-export const signup = (req: Request, res: Response) => {
-  res.send('signup');
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const userRepo = getRepository(User);
+    const user: User = await userRepo.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(200).json({ status: false, msg: 'User already exist' });
+    }
+
+    const newUser = userRepo.create(req.body);
+    const results: User[] = await userRepo.save(newUser);
+
+    return res
+      .status(201)
+      .json({ status: true, msg: 'User created successfully.', data: results });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, msg: 'something went wrong', data: null, error });
+  }
 };
 
 export const login = (req: Request, res: Response) => {
