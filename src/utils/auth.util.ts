0923fdf8a7f8c8bd8684fa75afefa513aaf583ed
Rohit Ambre <rohit003.ambre@gmail.com>
2020-10-07 01:39:37 +0530

    docs: added jsdocs for util functions

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -1,6 +1,12 @@
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 
+/**
+ * compare entered password with user's password
+ * @param enteredPwd user entered password
+ * @param hashedPwd hashed password from DB
+ * @returns promise
+ */
 export const comparePassword = async (
   enteredPwd: string,
   hashedPwd: string
@@ -8,6 +14,11 @@ export const comparePassword = async (
   return compare(enteredPwd, hashedPwd);
 };
 
+/**
+ * created JWT token from payload
+ * @param payload to be saved in JWT token
+ * @returns promise
+ */
 export const createAccessToken = async (payload: object) => {
   const expiry: string = '15m';
   return sign(payload, process.env.JWT_SECRET, { expiresIn: expiry });
